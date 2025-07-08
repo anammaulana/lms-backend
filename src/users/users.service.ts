b18073ee67e4a3c211f099567de3a87b0d6d65ac
anammaulana <anammaulana210447@gmail.com>
2025-07-08 16:24:07 +0700

    feat : Integrate Prisma ORM with NestJS application
    Added Prisma schema and initial migration for the User model, and integrated Prisma as a global module in the NestJS app. Updated UsersService to use PrismaService for database access. Adjusted .gitignore for generated Prisma client output.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './entity/user.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
     private users: User[] = [];
+    constructor(private prisma: PrismaService){}
 
     findAll(): User[] {
         return this.users;
@@ -12,6 +14,10 @@ export class UsersService {
     findByEmail(email: string): User | undefined {
         return this.users.find(user => user.email === email);
     }
+
+    async getAllUsers() {
+        return this.prisma.user.findMany();
+      }
 
     create(user: User): User {
         this.users.push(user);
