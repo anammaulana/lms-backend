c98599c8dc47d4c6707a9b655c7b9c128cba1f54
anammaulana <anammaulana210447@gmail.com>
2025-07-09 12:09:49 +0700

    Enable CORS with custom configuration
    CORS is now enabled with support for credentials, specific HTTP methods, and allowed headers. This change allows the backend to accept requests from the frontend and handle cookies or sessions.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true, // ganti dengan alamat frontend kamu
+    credentials: true, // jika pakai cookie/session
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   const config = new DocumentBuilder()
     .setTitle('LMS SERVICE API')
     .setDescription('Dokumentasi RESTful API untuk LMS')
